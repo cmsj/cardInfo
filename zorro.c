c847c0d7348202fdfb736d1b0b338cad7bcd489d
Chris Jones <cmsj@tenshu.net>
2016-08-22 20:58:23 +0200

    tidy up zorro header and add in zorro checking

--- zorro.c
+++ zorro.c
@@ -8,14 +8,13 @@
 #include <clib/exec_protos.h>
 #include <clib/expansion_protos.h>
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
+#include "zorro.h"
 
 struct Library *ExpansionBase;
 
 int search_zorro(char option, BOOL testExpected, char *testValue) {
-  struct ConfigDev *myCD;
+  BOOL returnCode = FALSE;
+  struct ConfigDev *myCD = NULL;
 
   UWORD m,t;
   UBYTE p;
@@ -24,76 +23,64 @@ int search_zorro(char option, BOOL testExpected, char *testValue) {
   char product[8] = "0";
   char type[20] = "Unknown";
 
-  /*
-  if (testExpected) {
-    switch ((unsigned int)option) {
-      case 'v':
-        if (!stringTest(manufacturer, testValue)) exitCode = EXIT_WARN;
-        break;
-      case 'p':
-        if (!stringTest(product, testValue)) exitCode = EXIT_WARN;
-        break;
-      case 't':
-        if (!stringTest(type, testValue)) exitCode = EXIT_WARN;
-        break;
-      case '1':
-        if (!stringTest(infoString1, testValue)) exitCode = EXIT_WARN;
-        break;
-      case '2':
-        if (!stringTest(infoString2, testValue)) exitCode = EXIT_WARN;
-        break;
-      case '3':
-        if (!stringTest(infoString3, testValue)) exitCode = EXIT_WARN;
-        break;
-      default:
-        error("Invalid option");
-        break;
-    }
-  } else {
-    switch ((unsigned int)option) {
-      case 'v':
-        printf("%s\n", manufacturer);
-        break;
-      case 'p':
-        printf("%s\n", product);
-        break;
-      case 't':
-        printf("%s\n", type);
-        break;
-      case '1':
-        printf("%s\n", infoString1);
-        break;
-      case '2':
-        printf("%s\n", infoString2);
-        break;
-      case '3':
-        printf("%s\n", infoString3);
-        break;
-      default:
-        printf("PCMCIA:%s:%s:%s:%s:%s:%s\n", manufacturer, product, type, infoString1, infoString2, infoString3);
-        break;
-    }
-  }
-  */
-
   if((ExpansionBase = OpenLibrary("expansion.library", 0L)) == NULL)
     return FALSE;
 
-    myCD = NULL;
-    while((myCD = FindConfigDev(myCD, -1L, -1L))) { /* search for all ConfigDevs */
-      m = myCD->cd_Rom.er_Manufacturer;
-      p = myCD->cd_Rom.er_Product;
-      t = myCD->cd_Rom.er_Type;
+  while((myCD = FindConfigDev(myCD, -1L, -1L))) { /* search for all ConfigDevs */
+    m = myCD->cd_Rom.er_Manufacturer;
+    p = myCD->cd_Rom.er_Product;
+    t = myCD->cd_Rom.er_Type;
 
-      snprintf(manufacturer, 8, "%ld", m);
-      snprintf(product, 8, "%ld", p);
-      snprintf(type, 8, "%ld", t);
+    snprintf(manufacturer, 8, "%ld", m);
+    snprintf(product, 8, "%ld", p);
+    snprintf(type, 8, "%ld", t);
 
-      printf("ZORRO:%s:%s:%s:::\n", manufacturer,
-                                    product,
-                                    type);
+    if (testExpected) {
+      switch ((unsigned int)option) {
+        case 'v':
+          if (stringTest(manufacturer, testValue)) returnCode = TRUE;
+          break;
+        case 'p':
+          if (stringTest(product, testValue)) returnCode = TRUE;
+          break;
+        case 't':
+          if (stringTest(type, testValue)) returnCode = TRUE;
+          break;
+        case '1':
+        case '2':
+        case '3':
+          break;
+        default:
+          error("Invalid option");
+          break;
       }
-    CloseLibrary(ExpansionBase);
+    } else {
+      switch ((unsigned int)option) {
+        case 'v':
+          printf("%s\n", manufacturer);
+          break;
+        case 'p':
+          printf("%s\n", product);
+          break;
+        case 't':
+          printf("%s\n", type);
+          break;
+        case '1':
+          printf("\n");
+          break;
+        case '2':
+          printf("\n");
+          break;
+        case '3':
+          printf("\n");
+          break;
+        default:
+          printf("ZORRO:%s:%s:%s:::\n", manufacturer, product, type);
+          break;
+      }
+    }
+  }
+  CloseLibrary(ExpansionBase);
 
-  return FALSE; // This is wrong
+  return returnCode;
 }
--- zorro.h
+++ zorro.h
@@ -1,15 +1,7 @@
-#include <exec/types.h>
-#include <exec/memory.h>
-#include <proto/dos.h>
-#include <proto/exec.h>
-#include <libraries/dos.h>
-#include <libraries/configvars.h>
-
-#include <clib/exec_protos.h>
-#include <clib/expansion_protos.h>
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+
+#include "util.h"
 
 int search_zorro(char option, BOOL testExpected, char *testValue);
